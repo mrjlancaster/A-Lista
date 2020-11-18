e58f02ac34a16b315e5b51b14f7d9ef2d48e49d6
mrjlancaster <lancast001@gmail.com>
2020-11-18 13:40:30 -0600

    delete blank space

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('input');
 const button = document.querySelector('form button');
 const ul = document.querySelector('ul');
-const guest = [];
+const guests = [];
 
 // get guests name and add to list
 button.addEventListener('click', () => {
@@ -10,14 +10,13 @@ button.addEventListener('click', () => {
 
     li.innerText = newGuest;
     // add to list
-    guest.push(newGuest);
+    guests.push(newGuest);
 
     // display list
     ul.appendChild(li);
 
-
     //clear input
     input.value = '';
 
-    console.log(guest);
+    console.log(guests);
 })
