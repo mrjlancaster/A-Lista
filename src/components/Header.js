b1e018b97a0d23827557c5a1606e10ce8f1d45e2
mrjlancaster <lancast001@gmail.com>
2020-11-30 17:05:03 -0600

    install react-dom dependency

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import '../components/Header.css';
+import { Link } from 'react-dom';
 
 const Header = () => {
     return (
@@ -6,8 +7,8 @@ const Header = () => {
             <h2>A-LISTA</h2>
             <navbar>
                 <ul>
-                    <li className='signIn'>Sign In</li>
-                    <li className='register'>Create your account</li>
+                    <Link className='signIn' to="/signIn">Sign In</Link>
+                    <Link className='register' to="/register">Create your account</Link>
                 </ul>
             </navbar>
         </div>
