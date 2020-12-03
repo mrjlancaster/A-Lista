953669721a68bb964dcd31cbf33fefb6ad12b5be
mrjlancaster <lancast001@gmail.com>
2020-12-02 19:21:58 -0600

    create register input

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,20 @@
 import './Body.css';
+import { Link } from 'react-router-dom';
 
 const Body = () => {
     return (
         <div className="body_container">
             <div className="body_description-wrapper">
                 <h1>Your guests deserve to be reserved</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur atque aperiam suscipit incidunt rerum explicabo, maiores hic. Mollitia fuga earum corporis dolores, fugit totam dolore dicta architecto explicabo voluptatibus repellat eaque quidem, nemo adipisci accusamus natus, dignissimos impedit vitae odit laboriosam </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur atque aperiam suscipit incidunt rerum explicabo, maiores hic. Mollitia fuga earum corporis dolores, fugit totam dolore dicta architecto</p>
             </div>
-
-
-            <label for="email">
-                <input type="text" name="email" placeholder="youremail@email.com" />
-                Create account
-            </label>
+            <div className="register_input-wrapper">
+                <div>
+                    <input type="text" className="register_input" name="email" placeholder="youremail@email.com" />
+                </div>
+                <Link to='/register' className="register_link">Create account<i class="fas fa-long-arrow-alt-right"></i></Link>
+            </div>
+      
         </div>
     )
 }
