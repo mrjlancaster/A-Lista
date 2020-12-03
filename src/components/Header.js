a5661b33db62d0d2789140e29401c1d18c8117bf
mrjlancaster <lancast001@gmail.com>
2020-12-02 19:44:38 -0600

    position content and input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,21 @@
 import '../components/Header.css';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <Router>
-            <div className='header'>
-                <h2>A-LISTA</h2>
-                <navbar>
-                    <ul>
-                        <li className='signIn'>
-                            <Link to='/signin' className="li-font">Sign In</Link>
-                        </li>
-                        <li className='register'>
-                            <Link to='/register' className="li-font">Create your account</Link>
-                        </li>
-                    </ul>
-                </navbar>
-            </div>
-        </Router>
+        <div className='header'>
+            <h2>A-LISTA</h2>
+            <navbar>
+                <ul>
+                    <li className='signIn'>
+                        <Link to='/signin' className="li-font">Sign In</Link>
+                    </li>
+                    <li className='register'>
+                        <Link to='/register' className="li-font">Create your account</Link>
+                    </li>
+                </ul>
+            </navbar>
+        </div>
     )
 }
 
