c14bf913125b6fb64065ec520187bfd42a73171a
mrjlancaster <lancast001@gmail.com>
2020-12-04 18:36:45 -0600

    position register sidebar items

--- src/layouts/Register.js
+++ src/layouts/Register.js
@@ -7,24 +7,32 @@ const Register = () => {
 		<div className="register_container">
 			<div className="register_links">
 				<h1>A-Lista</h1>
-				<h3>New User Registration</h3>
-				<ul>
-					<li>
-						<Link to='/login'>Login</Link>
-					</li>
-					<li>
-						<Link to='/register'>Register</Link>
-					</li>
-				</ul>
+
+				<div className="newUserRegistration">
+					<h3>
+						New User <br />
+						Registration
+					</h3>
+					<ul>
+						<li>
+							<Link to='/login'>Login</Link>
+						</li>
+						<li>
+							<Link to='/register'>Register</Link>
+						</li>
+					</ul>
+
+				</div>
+		
 			</div>
 
 			<div className="register_form-container">
 				<form action="/" className="register_form" method="POST">
 					<h1>Register</h1>
 					<div className="register_inputs-container">
-						<input type="text" placeholder="Full name" />
-						<input type="email" placeholder="Email" />
-						<input type="password" placeholder="Password" />
+						<input type="text" placeholder="Full name*" />
+						<input type="email" placeholder="Email*" />
+						<input type="password" placeholder="Password*" />
 					</div>
 					<button type="submit">Create account</button>
 					<p>Already have an account?
