a76899d0f45900dfab9a36fc7fe23ebe7ddd67af
mrjlancaster <lancast001@gmail.com>
2020-12-07 14:51:37 -0600

    delete unnecessary div

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,9 @@ const Header = () => {
 			<h2>
 				<Link to="/">A-LISTA</Link>
 			</h2>
-			<nav>
-				<div className="navigation_links">
-					<Link to='/login' className="li-font signIn">Login</Link>
-					<Link to='/register' className="li-font register">Create your account</Link>
-				</div>
+			<nav className="navigation_links">
+				<Link to='/login' className="li-font signIn">Login</Link>
+				<Link to='/register' className="li-font register">Create your account</Link>
 			</nav>
 		</div>
 	)
