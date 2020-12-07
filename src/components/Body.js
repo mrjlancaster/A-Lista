7bbb574f60aa3287a8b5330ea64785ecc0f26749
mrjlancaster <lancast001@gmail.com>
2020-12-07 12:35:06 -0600

    remove blank space

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,14 +3,12 @@ import './Body.css';
 import { Link } from 'react-router-dom';
 
 const Body = () => {
-
 	const [ email, setEmail ] = useState('');
 
 	const handleEmail = () => {
 		setEmail('');
 		console.log(email);
 	}
-
 
 	return (
 		<div className="body_container">
@@ -19,10 +17,12 @@ const Body = () => {
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur atque aperiam suscipit incidunt rerum explicabo, maiores hic. Mollitia fuga earum corporis dolores, fugit totam dolore dicta architecto</p>
 			</div>
 			<div className="register_input-wrapper">
-					<div>
-						<input type="text" className="register_input" name="email" placeholder="emailadress@example.com" onChange={(e) => setEmail(e.target.value)} value={email} />
-					</div>
-						<Link onClick={handleEmail} to='/register' className="register_link">Create account <i class="fas fa-long-arrow-alt-right"></i></Link>
+				<div>
+					<input type="text" className="register_input" name="email" placeholder="emailadress@example.com" onChange={(e) => setEmail(e.target.value)} value={email} />
+				</div>
+					<Link onClick={handleEmail} to='/register' className="register_link">
+						Create account <i class="fas fa-long-arrow-alt-right"></i>
+					</Link>
 			</div>
 		</div>
 	)
