d2e4939a941fca2545adf1c6e49a0ceacc6ad914
mrjlancaster <lancast001@gmail.com>
2020-12-27 15:38:07 -0600

    create landing page layout

--- src/layouts/LandingPage.js
+++ src/layouts/LandingPage.js
@@ -5,18 +5,18 @@ const LandingPage = () => {
 		<div className="landing__page">
 			<div className="landing__page--content">
 				<h2 className="coming__soon--title">A-LISTA</h2>
-				<p className="coming__soon">Coming soon</p>
-				<p className="hashtag">#Underconstruction</p>
-				<div>
-					<p>Get notify when it's ready</p>
-					<div className="notify__me--wrapper">
-						<input type="text" className="notify__me--input" name="email" placeholder="emailadress@example.com"  />
-						<button type="button" className="get__notified--btn">
-							Create account <i className="fas fa-long-arrow-alt-right"></i>
-						</button>
-
-					</div>
+				<div className="coming__soon--texts">
+					<p className="hashtag">#Underconstruction</p>
+					<p className="coming__soon">Coming soon</p>
 				</div>
+
+				<div className="notify__me--wrapper">
+					<input type="text" className="notify__me--input" name="email" placeholder="youremail@example.com"  />
+					<button type="button" className="get__notified--btn">
+						Get Notified <i className="fas fa-long-arrow-alt-right"></i>
+					</button>
+				</div>
+				
 			</div>
 			<p className="copyrights">Copyright &copy; 2020 All Rights Reserved</p>
 		</div>
