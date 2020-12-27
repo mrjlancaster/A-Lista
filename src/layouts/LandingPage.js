7ebc231d467f55480a285e254e621d2518cb06f0
mrjlancaster <lancast001@gmail.com>
2020-12-27 14:35:25 -0600

    add content to landing page

--- src/layouts/LandingPage.js
+++ src/layouts/LandingPage.js
@@ -1,7 +1,23 @@
+import Background from '../img/landing-page-bg2.jpg';
+import './LandingPage.css';
+
 const LandingPage = () => {
 	return (
-		<div>
-			This is the landing page
+		<div className="landing__page">
+			<div className="landing__page--content">
+				<h2 className="coming__soon--title">A-LISTA</h2>
+				<p className="coming__soon">Coming soon</p>
+				<p className="hashtag">#Underconstruction</p>
+				<div>
+					<p>Get notify when it's ready</p>
+					<div>
+						<input type="text" className="register_input" name="email" placeholder="emailadress@example.com"  />
+
+						<p className="copyrights">Copyright &copy; 2020 All Rights Reserved</p>
+					</div>
+				</div>
+			</div>
+			
 		</div>
 	)
 }
